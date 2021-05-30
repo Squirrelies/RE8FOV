769771ea531e158a83a0db90ea0fe4f981cf9e0d
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2021-05-29 23:13:50 -0500

    v1.0.1.0 Release

---
+++ RE8FOV/PInvoke/Kernel32.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RE8FOV.PInvoke
+{
+    public static class Kernel32
+    {
+        [DllImport(nameof(Kernel32), SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessRightsFlags dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport(nameof(Kernel32), SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, UIntPtr nSize, out UIntPtr lpNumberOfBytesRead);
+
+        [DllImport(nameof(Kernel32), SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static unsafe extern bool ReadProcessMemory(IntPtr hProcess, void* lpBaseAddress, void* lpBuffer, UIntPtr nSize, out UIntPtr lpNumberOfBytesRead);
+
+        [DllImport(nameof(Kernel32), SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, UIntPtr nSize, out UIntPtr lpNumberOfBytesRead);
+
+        [DllImport(nameof(Kernel32), SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static unsafe extern bool WriteProcessMemory(IntPtr hProcess, void* lpBaseAddress, void* lpBuffer, UIntPtr nSize, out UIntPtr lpNumberOfBytesRead);
+
+        // ACCESS_MASK documentation: https://docs.microsoft.com/en-us/windows/win32/secauthz/access-mask-format
+        [Flags]
+        public enum GenericAccessRightsFlags : uint
+        {
+            ALL = 1U << 28, // 0x10000000,
+            EXECUTE = 1U << 29, // 0x20000000,
+            WRITE = 1U << 30, // 0x40000000,
+            READ = 1U << 31, // 0x80000000,
+        }
+
+        [Flags]
+        public enum StandardAccessRightsFlags : uint
+        {
+            DELETE = 1U << 16, // 0x00010000,
+            READ_CONTROL = 1U << 17, // 0x00020000,
+            WRITE_DAC = 1U << 18, // 0x00040000,
+            WRITE_OWNER = 1U << 19, // 0x00080000,
+
+            SYNCHRONIZE = 1U << 20, // 0x00100000,
+            // = 1U << 21, // 0x00200000,
+            // = 1U << 22, // 0x00400000,
+            // = 1U << 23, // 0x00800000,
+
+            ACCESS_SYSTEM_SECURITY = 1U << 24, // 0x01000000,
+            MAXIMUM_ALLOWED = 1U << 25, // 0x02000000,
+            // = 1U << 26, // 0x04000000,
+            // = 1U << 27, // 0x08000000,
+
+            STANDARD_RIGHTS_ALL = DELETE | READ_CONTROL | WRITE_DAC | WRITE_OWNER | SYNCHRONIZE, // 0x001F0000,
+            STANDARD_RIGHTS_EXECUTE = READ_CONTROL, // 0x00020000,
+            STANDARD_RIGHTS_READ = READ_CONTROL, // 0x00020000,
+            STANDARD_RIGHTS_REQUIRED = DELETE | READ_CONTROL | WRITE_DAC | WRITE_OWNER, // 0x000F0000,
+            STANDARD_RIGHTS_WRITE = READ_CONTROL, // 0x00020000,
+        }
+
+        [Flags]
+        public enum ProcessAccessRightsFlags : uint
+        {
+            TERMINATE = 1U << 0, // 0x00000001,
+            CREATE_THREAD = 1U << 1, // 0x00000002,
+            // = 1U << 2, // 0x00000004,
+            VM_OPERATION = 1U << 3, // 0x00000008,
+
+            VM_READ = 1U << 4, // 0x00000010,
+            VM_WRITE = 1U << 5, // 0x00000020,
+            DUP_HANDLE = 1U << 6, // 0x00000040,
+            CREATE_PROCESS = 1U << 7, // 0x00000080,
+
+            SET_QUOTA = 1U << 8, // 0x00000100,
+            SET_INFORMATION = 1U << 9, // 0x00000200,
+            QUERY_INFORMATION = 1U << 10, // 0x00000400,
+            SUSPEND_RESUME = 1U << 11, // 0x00000800,
+
+            QUERY_LIMITED_INFORMATION = 1U << 12, // 0x00001000,
+            // = 1U << 13, // 0x00002000,
+            // = 1U << 14, // 0x00004000,
+            // = 1U << 15, // 0x00008000,
+        }
+    }
+}
--- RE8FOV/ProcessMemory.cs
+++ RE8FOV/ProcessMemory.cs
@@ -4,14 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
+using static RE8FOV.PInvoke.Kernel32;
 
 namespace RE8FOV
 {
-    public class ProcessMemory
+    public unsafe class ProcessMemory
     {
-        private PInvoke.Kernel32.SafeObjectHandle processHandle;
-        private long processBaseAddress;
-        private long basePointer;
+        private IntPtr processHandle;
+        private byte* basePointer; // Done as a byte* instead of long* because + 1 on the pointer will advance it by 8 (size of a long).
         private int[] fovOffsets = new int[] { 0x1F8, 0x68, 0x78, 0x128, 0x20, 0x20, 0x50 };
 
         public ProcessMemory(string processName)
@@ -19,8 +19,8 @@ namespace RE8FOV
             byte[] checksum;
             using (Process proc = Process.GetProcessesByName(processName)[0])
             {
-                this.processHandle = PInvoke.Kernel32.OpenProcess(PInvoke.Kernel32.ProcessAccess.PROCESS_QUERY_INFORMATION | PInvoke.Kernel32.ProcessAccess.PROCESS_VM_READ | PInvoke.Kernel32.ProcessAccess.PROCESS_VM_WRITE, false, proc.Id);
-                this.processBaseAddress = proc.MainModule.BaseAddress.ToInt64();
+                this.processHandle = OpenProcess(ProcessAccessRightsFlags.QUERY_INFORMATION | ProcessAccessRightsFlags.VM_READ | ProcessAccessRightsFlags.VM_WRITE, false, proc.Id);
+                this.basePointer = (byte*)proc.MainModule.BaseAddress.ToPointer();
 
                 using (SHA256 hashFunc = SHA256.Create())
                 using (FileStream fs = new FileStream(proc.MainModule.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
@@ -29,76 +29,53 @@ namespace RE8FOV
 
             // Checksum check to determine which base pointer to use.
             if (checksum.SequenceEqual(GameHashes.re8WW_20210506_1))
-                this.basePointer = 0x0A1A74F0L;
+                this.basePointer += 0x0A1A74F0L;
             else if (checksum.SequenceEqual(GameHashes.re8ceroD_20210506_1))
-                this.basePointer = 0x0A1A74F0L + 0x2000L;
+                this.basePointer += 0x0A1A74F0L + 0x2000L;
             else if (checksum.SequenceEqual(GameHashes.re8ceroZ_20210508_1))
-                this.basePointer = 0x0A1A74F0L + 0x1000L;
+                this.basePointer += 0x0A1A74F0L + 0x1000L;
             else if (checksum.SequenceEqual(GameHashes.re8promo01_20210426_1))
-                this.basePointer = 0x0A1A74F0L + 0x1030L;
+                this.basePointer += 0x0A1A74F0L + 0x1030L;
             else
                 throw new FileFormatException("re8.exe SHA256 checksum not recognized! Possible new version or modified game file!");
         }
 
-        private unsafe long GetPlayerCameraParameterAddress()
+        private unsafe bool GetPlayerCameraParameterAddress(out long address)
         {
-            long address = 0L;
-            UIntPtr bytesRead = UIntPtr.Zero;
-            int lastError = 0;
-
-            if (!PInvoke.Kernel32.ReadProcessMemory(processHandle, new IntPtr(processBaseAddress + basePointer), new IntPtr(&address), new UIntPtr((uint)sizeof(IntPtr)), out bytesRead))
+            fixed (long* addressPointer = &address)
             {
-                lastError = Marshal.GetLastWin32Error();
+                if (!ReadProcessMemory(processHandle, basePointer, addressPointer, (UIntPtr)sizeof(long), out UIntPtr _))
+                    return false;
+
+                for (int i = 0; i < fovOffsets.Length; ++i)
+                    if (!ReadProcessMemory(processHandle, (byte*)address + fovOffsets[i], addressPointer, (UIntPtr)sizeof(long), out UIntPtr _))
+                        return false;
             }
 
-            for (int i = 0; i < fovOffsets.Length; ++i)
-            {
-                if (!PInvoke.Kernel32.ReadProcessMemory(processHandle, new IntPtr(address + fovOffsets[i]), new IntPtr(&address), new UIntPtr((uint)sizeof(IntPtr)), out bytesRead))
-                {
-                    lastError = Marshal.GetLastWin32Error();
-                }
-            }
-
-            return address;
+            return true;
         }
 
-        public unsafe float[] GetFOVValues()
+        public unsafe bool GetFOVValues(out float[] values)
         {
-            float[] value = new float[2];
-            UIntPtr bytesRead = UIntPtr.Zero;
-            int lastError = 0;
-
-            fixed (float* f = value)
-            {
-                if (!PInvoke.Kernel32.ReadProcessMemory(processHandle, new IntPtr(GetPlayerCameraParameterAddress() + 0x38), (IntPtr)f, (UIntPtr)8, out bytesRead))
-                {
-                    lastError = Marshal.GetLastWin32Error();
-                }
-            }
-
-            return value;
+            values = new float[2];
+            if (GetPlayerCameraParameterAddress(out long address))
+                fixed (float* f = values)
+                    return ReadProcessMemory(processHandle, (byte*)address + 0x38L, f, (UIntPtr)(sizeof(float) * 2), out UIntPtr _);
+            else
+                return false;
         }
 
         public unsafe bool SetFOVValues(float normal = 81f, float aiming = 70f)
         {
-            UIntPtr bytesRead = UIntPtr.Zero;
-            try
+
+            if (GetPlayerCameraParameterAddress(out long address))
             {
-                bool success = false;
-                processHandle.DangerousAddRef(ref success);
-                if (success)
-                {
-                    float[] values = new float[2] { normal, aiming };
-                    fixed (float* f = values)
-                        return PInvoke.Kernel32.WriteProcessMemory(processHandle.DangerousGetHandle(), new IntPtr(GetPlayerCameraParameterAddress() + 0x38), (IntPtr)f, (UIntPtr)8, out bytesRead);
-                }
-                else
-                    return false;
+                float[] values = new float[2] { normal, aiming };
+                fixed (float* f = values)
+                    return WriteProcessMemory(processHandle, (byte*)address + 0x38L, f, (UIntPtr)(sizeof(float) * 2), out UIntPtr _);
             }
-            finally
-            {
-                processHandle.DangerousRelease();
-            }
+            else
+                return false;
         }
     }
 }
